289bd9f79b3ccabfe5034ac2c7436847d3b44494
Jeff Beck <beckje01@gmail.com>
2015-07-29 14:51:23 -0500

    Interact with a session

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,26 +1,52 @@
+import ratpack.session.Session
+
 import static ratpack.groovy.Groovy.ratpack
+import ratpack.session.SessionModule
 
 ratpack {
-  bindings {
+	bindings {
+		module SessionModule
+	}
 
-  }
+	handlers {
+		all {
+			if (request.headers['Authorization'] != "Token faketoken") {
+				response.status(401)
+				//We must send some response or the request will hang.
+				response.send()
+			} else {
+				//We can choose to do nothing but allow the next handler in the chain to deal with the request.
+				def session = context.get(Session)
 
-  handlers {
-    all {
-      if (request.headers['Authorization'] != "Token faketoken") {
-        response.status(401)
-        //We must send some response or the request will hang.
-        response.send()
-      } else {
-        //We can choose to do nothing but allow the next handler in the chain to deal with the request.
-        println "next"
-        next()
-      }
-    }
+				//Set some session data if we don't already have it
+				session.get("example").then { val ->
+					session.set("example", val.orElse("Galaxy")).then {
+						next()
+					}
+				}
 
-    all {
-      println "hellow"
-      render "Hello World"
-    }
-  }
+			}
+		}
+
+		get("add") { Session session ->
+			session.get("example").then {
+				def str = it.orElse("Pluto") + "+1"
+				session.set("example", str).then {
+					render "Set: " + str
+				}
+			}
+		}
+
+		get("term") { Session session ->
+			session.terminate().then {
+				render "Terminated the session"
+			}
+		}
+
+		all {
+			context.get(Session).get("example").then { value ->
+				render "Hello " + value.orElse("World")
+			}
+		}
+	}
 }
